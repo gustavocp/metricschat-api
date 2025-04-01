2974539fea1906ca51a6bafc2712f87e461b933e
gustavocp <gustavocarvalhopereira@gmail.com>
2025-04-01 17:31:14 -0300

    Facebook Ads Api V1, somente listar campaigns e connect

--- facebook.js
+++ facebook.js
@@ -11,8 +11,19 @@ const swaggerJSDoc = require('swagger-jsdoc');
 //    - Verifique se esse JSON está no mesmo local, com nome correto
 //    - Certifique-se de que a conta de serviço tem permissão no Firestore
 // ---------------------------------------------------------------------
-const serviceAccount = require('./configs/metricschat-firebase-adminsdk-njs58-978151d9af.json');
-
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   // Se quiser usar Real Time Database, adicione databaseURL
